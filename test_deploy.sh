7c5d23ee945e14ff821ff34326f927a3bf5ebf3a
Matheus Carino <eu@matheuscarino.com.br>
2016-04-12 20:13:18 -0300

    Updated test_deploy.sh script

--- test_deploy.sh
+++ test_deploy.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 set -x
-DATE=`date +"%d-%m-%Y"`
-MYHOME="/home/ec2-user/"
-FILENAME="wasp-api"
+CHECK=$1
+DATE=`date +"%Y-%m-%d-%H-%M"`
 DEPLOYDIR="/domains/WEBSUPPLY_PRD/servers/AdminServer/upload"
-echo "Criando cópia de Segurança"
-mv $DEPLOYDIR/$FILENAME $MYHOME/$FILENAME-$DATE
-echo "Fazendo Deploy"
-mv $MYHOME/$FILENAME $DEPLOYDIR/
+MYHOME="/root/"
+FILENAME="teste"
+VALOR=`md5sum $MYHOME$FILENAME | awk -F" " '{print $1}'`
+if [ $VALOR = $CHECK ] ; then
+        echo "OK"
+        echo "Criando cópia de Segurança"
+        mv $DEPLOYDIR/$FILENAME $MYHOME/$FILENAME-$DATE
+        echo "Fazendo Deploy"
+        mv $MYHOME/$FILENAME $DEPLOYDIR/
+else
+        echo "FAIL"
+fi
